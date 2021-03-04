97fd3b1f2f22adbf92a7571cffe992fe03582d48
Le-DarkOverlord <james_harvey@hotmail.ca>
2021-03-03 22:44:39 -0600

    Added main API calls needed to interact with stack overflow

--- so-questions-app/src/App.js
+++ so-questions-app/src/App.js
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import './App.css';
-//import { getTopVoted } from './getTagQuestions.js';
+import { getQuestions } from './getPostInfo.js';
 
 function App() {
-  //const { topVotesQuestions } = getTopVoted(tag)
   const [tag, setTag] = useState('')
-  const questions = ['Question 1','Question 2','Question 3','Question 4','Question 5','Question 6',
+  const [questions, setQuestions] = useState([])
+  var questionst = ['Question 1','Question 2','Question 3','Question 4','Question 5','Question 6',
   'Question 7','Question 8','Question 9','Question 10','Question 11','Question 12','Question 13',
   'Question 14','Question 15','Question 16','Question 17','Question 18','Question 19','Question 20'] 
 
   const handleSubmit = (event) => {
     event.preventDefault()
     setTag(event.target.innerText)
-    //setQuestion(getTopVoted(tag),getTopRecent(tag))
+    const timeStamp = Math.floor(Date.now() / 1000)
+    var timeStamp2 = new Date();
+    timeStamp2.setDate(timeStamp2.getDate() - 7)
+
+    // Promise.all([getQuestions(tag,0,0,'votes'), getQuestions(tag,0,0,'')]).then(([topVoted, topNewest]) => {
+    //   setQuestions(topVoted+topNewest)
+    // })
   }
 
   const handleClick = (event) => {
     event.target.classList.toggle("active")
     var content = event.target.nextElementSibling
+    //var id = event.target.question_id
     if(content.style.maxHeight){
       content.style.maxHeight = null
     }
     else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
-
+    // Promise.all([getQuestionAnswers(id), getQuestionComments(id)])
+    // .then(([questionAnswers, questionComments]) => {
+    //   setQuestionAnswers(questionAnswers)
+    //   setQuestionComments(questionComments) 
+    // })
+    // foreach(answer in questionAnswers) {
+    //  wrong --> getAnswerComments(answer.answer_id)
+    //}
   }
 
   return (
@@ -36,8 +50,8 @@ function App() {
             <div><input type="submit" className="search_box_submit" value="Search Stack Overflow"/></div>
         </div>
       </form>
-      {questions.map(question => 
-      <div>
+      {questionst.map(question => 
+      <div key={question.title}>
         <button className="collapsible" onClick={handleClick}>{question}</button>
         <div className="content">
           <p>I have a question about a thing?</p>
---
+++ so-questions-app/src/getPostInfo.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+
+export const getQuestions = async(tag, startdate, endDate, sortBy) => {
+    try {
+        const resp = await axios.get(`https://api.stackexchange.com/2.2/questions?order=desc&sort=${sortBy}&tagged=${tag}&site=stackoverflow&key=t29gugj)zVXNQJ5n)lgk0Q((`)
+        const statusCode = resp.status
+        const data = resp.data
+        var questions = {}
+        switch(statusCode) {
+            case 200: //OK
+                console.log("OK")
+                questions = data.items
+                break
+            case 400: //Bad Parameter
+                console.log("Bad Parameter")
+                break
+            case 404: //No Method
+                console.log("No Method")
+                break
+            case 409: //Duplicate Request
+                console.log("Duplicate Request")
+                break
+            case 500: //Internal Error
+                console.log("Internal Error")
+                break
+            case 502: //Throttle Violation
+                console.log("Throttle Violation")
+                break
+            case 503: //Temporarily Unavailable
+                console.log("Temporarily Unavailable")
+                //backoff
+                break
+            default:
+                break
+        }
+        return questions
+    } catch(error) {
+            return false
+    }
+}
+
+export const getQuestionAnswers = async(id) => {
+    try {
+        const resp = await axios.get(``)
+        const statusCode = resp.status
+        const data = resp.data
+        var questionAnswers = {}
+        switch(statusCode) {
+            case 200: //OK
+                console.log("OK")
+                questionAnswers = data.items
+                break
+            case 400: //Bad Parameter
+                console.log("Bad Parameter")
+                break
+            case 404: //No Method
+                console.log("No Method")
+                break
+            case 409: //Duplicate Request
+                console.log("Duplicate Request")
+                break
+            case 500: //Internal Error
+                console.log("Internal Error")
+                break
+            case 502: //Throttle Violation
+                console.log("Throttle Violation")
+                break
+            case 503: //Temporarily Unavailable
+                console.log("Temporarily Unavailable")
+                //backoff
+                break
+            default:
+                break
+        }
+        return questionAnswers
+    } catch(error) {
+            return false
+    }
+}
+
+export const getQuestionComments = async(id) => {
+    try {
+        const resp = await axios.get(``)
+        const statusCode = resp.status
+        const data = resp.data
+        var questionComments = {}
+        switch(statusCode) {
+            case 200: //OK
+                console.log("OK")
+                questionComments = data.items
+                break
+            case 400: //Bad Parameter
+                console.log("Bad Parameter")
+                break
+            case 404: //No Method
+                console.log("No Method")
+                break
+            case 409: //Duplicate Request
+                console.log("Duplicate Request")
+                break
+            case 500: //Internal Error
+                console.log("Internal Error")
+                break
+            case 502: //Throttle Violation
+                console.log("Throttle Violation")
+                break
+            case 503: //Temporarily Unavailable
+                console.log("Temporarily Unavailable")
+                //backoff
+                break
+            default:
+                break
+        }
+        return questionComments
+    } catch(error) {
+            return false
+    }
+}
+
+export const getAnswerComments = async(id) => {
+    try {
+        const resp = await axios.get(``)
+        const statusCode = resp.status
+        const data = resp.data
+        var answerComments = {}
+        switch(statusCode) {
+            case 200: //OK
+                console.log("OK")
+                answerComments = data.items
+                break
+            case 400: //Bad Parameter
+                console.log("Bad Parameter")
+                break
+            case 404: //No Method
+                console.log("No Method")
+                break
+            case 409: //Duplicate Request
+                console.log("Duplicate Request")
+                break
+            case 500: //Internal Error
+                console.log("Internal Error")
+                break
+            case 502: //Throttle Violation
+                console.log("Throttle Violation")
+                break
+            case 503: //Temporarily Unavailable
+                console.log("Temporarily Unavailable")
+                //backoff
+                break
+            default:
+                break
+        }
+        return answerComments
+    } catch(error) {
+            return false
+    }
+}
